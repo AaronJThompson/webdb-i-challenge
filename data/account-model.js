3a00fec7612887000515c17b99bdee0005a63625
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-16 08:50:32 +0100

    Add db helpers

--- data/account-model.js
+++ data/account-model.js
@@ -6,6 +6,23 @@ function find() {
 
 function findById(id) {
     return db('accounts')
-        .select()
-        .where({ id });
+        .where({ id })
+        .first();
+    }
+
+function insert({ name, budget }) {
+    return db('accounts')
+        .insert({ name, budget });
+}
+
+function update(id, fields) {
+    return db('accounts')
+        .where({ id })
+        .update(fields);
+}
+
+function remove(id) {
+    return db('accounts')
+        .where({ id })
+        .del();
 }
